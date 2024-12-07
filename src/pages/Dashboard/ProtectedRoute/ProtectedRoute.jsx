f7a3bc942a21d4c54c60bd04a31b1605befbbd6c
Safayethossain95 <safayet.hossain95@gmail.com>
2024-12-07 15:51:54 +0600

    protected route

--- src/pages/Dashboard/ProtectedRoute/ProtectedRoute.jsx
+++ src/pages/Dashboard/ProtectedRoute/ProtectedRoute.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MyNavbarDesk from '../../../Components/Desktop/MyNavbarDesk';
 import { useAuth } from '../../../context/AuthContext';
 
 const ProtectedRoute = ({ element, ...rest }) => {
     const {isLogin} = useAuth()
-  if (!isLogin) {
-    return (
+    useEffect(() => {
+      // You can log or debug any state here to check for issues
+      console.log(isLogin);
+    }, [isLogin]);
+  
+    if (!isLogin) {
+      return (
         <div className="desktop">
           <MyNavbarDesk />
-          <div className="errorpage" style={{ height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <div
+            className="errorpage"
+            style={{
+              height: "100vh",
+              width: "100vw",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <div className="div text-center" style={{ height: "200px", width: "500px" }}>
               <h1>Access Denied!</h1>
               <p>Login First.</p>
@@ -17,8 +31,10 @@ const ProtectedRoute = ({ element, ...rest }) => {
         </div>
       );
     }
-  
-    return element; 
+    else{
+
+      return element; 
+    }
 };
 
 export default ProtectedRoute;

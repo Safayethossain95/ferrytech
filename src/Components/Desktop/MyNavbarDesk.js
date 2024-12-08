1773e9d90fc2494b66d5c371bc5d77b2e5af3c2a
Safayethossain95 <safayet.hossain95@gmail.com>
2024-12-08 10:27:31 +0600

    vercel.json

--- src/Components/Desktop/MyNavbarDesk.js
+++ src/Components/Desktop/MyNavbarDesk.js
@@ -31,6 +31,7 @@ const MyNavbarDesk = () => {
       
       document.cookie = "jwtToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure; SameSite=None";
       navigate("/login")
+      
     }
     logout()
   }
